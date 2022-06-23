3c61d54e48c5c8d73bfb3b2571d74fc57ac7613a
digitalmediatnt <100731265+digitalmediatnt@users.noreply.github.com>
2022-06-23 16:43:34 +0700

    commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../views/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <title>FriendChise</title>
   </head>
@@ -45,7 +45,7 @@
 <!-- Header Starts -->
 
 <div class="background-header">
-  <img src="assets/images/HeaderBackground.png" alt="HeaderBackground" class="HB">
+  <img src="images/HeaderBackground.png" alt="HeaderBackground" class="HB">
 </div>
 
 <!-- Header Ends -->

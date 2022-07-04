1e2c909f721e2f1c727cbc76e1c2513c307fa9f4
Nrulafind <mnurula.nu246@gmail.com>
2022-07-04 12:46:53 +0700

    commit

---
+++ application/controllers/News.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Homepage extends CI_Controller
+{
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    //     if ($this->session->userdata('status') !== 'login') {
+    //         $this->load->view('auth/login');
+    //         redirect('/');
+    //     }
+    //     $this->load->model('HomepageModel');
+    // }
+}

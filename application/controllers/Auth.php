4fc0ad4e650f6c31b20a481b42222ff4b510743d
Nrulafind <mnurula.nu246@gmail.com>
2022-07-04 14:03:41 +0700

    commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,38 +13,38 @@ class Auth extends CI_Controller
     public function login()
     {
         if ($this->session->userdata('status') === 'login') {
-            redirect('Homepage');
+            redirect('HomepageAfter');
         }
-        if ($username = $this->input->post('username')) {
+        if ($email = $this->input->post('email')) {
             $password = $this->input->post('password');
-            echo json_encode($this->AuthModel->login($username, $password));
+            echo json_encode($this->AuthModel->login($email, $password));
         } else {
             $this->load->view('auth/login');
         }
     }
 
-    // public function register()
-    // {
-    //     if ($this->session->userdata('status') === 'login') {
-    //         redirect('auth/login');
-    //     }
-    //     if ($username = $this->input->post('username')) {
-    //         $password = $this->input->post('password');
-    //         $email = $this->input->post('email');
-    //         if ($user = $this->auth_model->register($username, $password, $email)) {
-    //             $this->session->set_userdata($user);
-    //             echo json_encode('sukses');
-    //         } else {
-    //             echo json_encode('gagal');
-    //         }
-    //     } else {
-    //         $this->load->view('auth/register');
-    //     }
-    // }
+    public function register()
+    {
+        if ($this->session->userdata('status') === 'login') {
+            redirect('auth/login');
+        }
+        if ($email = $this->input->post('email')) {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            if ($user = $this->auth_model->register($email, $username, $password)) {
+                $this->session->set_userdata($user);
+                echo json_encode('sukses');
+            } else {
+                echo json_encode('gagal');
+            }
+        } else {
+            $this->load->view('auth/register');
+        }
+    }
 
-    // public function logout()
-    // {
-    //     $this->session->sess_destroy();
-    //     redirect('Homepage');
-    // }
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        redirect('HomepageBefore');
+    }
 }
--- application/controllers/Franchise.php
+++ application/controllers/Franchise.php
@@ -3,16 +3,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Franchise extends CI_Controller
 {
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     if ($this->session->userdata('status') === 'login') {
-    //         redirect('Franchise');
-    //         $this->load->model('FranchiseModel');
-    //     } else {
-    //         redirect('auth/login');
-    //     }
-    // }
+    public function __construct()
+    {
+        parent::__construct();
+        if ($this->session->userdata('status') === 'login') {
+            redirect('Franchise');
+            $this->load->model('FranchiseModel');
+        } else {
+            redirect('auth/login');
+        }
+    }
     public function index()
     {
         $this->load->view('Franchise/index');

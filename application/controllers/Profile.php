e9e27d539699c69f114dfb8d4898e8a3dcadb176
Nrulafind <mnurula.nu246@gmail.com>
2022-07-04 15:03:25 +0700

    commit

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class AboutUs extends CI_Controller
+class Profile extends CI_Controller
 {
     public function index()
     {

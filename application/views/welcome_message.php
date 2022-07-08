73e14b2da5a94ad743013e1e1cc0da577d6f7c06
Nrulafind <mnurula.nu246@gmail.com>
2022-07-08 20:53:13 +0700

    commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -10,7 +10,17 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php echo base_url('./assets/css/style.css'); ?>">
 
-	<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="8bb95c6d-9134-49c4-b9df-ab402a708c6d";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+	<script type="text/javascript">
+		window.$crisp = [];
+		window.CRISP_WEBSITE_ID = "8bb95c6d-9134-49c4-b9df-ab402a708c6d";
+		(function() {
+			d = document;
+			s = d.createElement("script");
+			s.src = "https://client.crisp.chat/l.js";
+			s.async = 1;
+			d.getElementsByTagName("head")[0].appendChild(s);
+		})();
+	</script>
 	<title>FriendChise</title>
 </head>
 
@@ -38,7 +48,7 @@
 						<a class="nav-link" href="<?php base_url(); ?>Profile">Profile</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link login" href="<?php base_url() ?>Auth/login">Login</a>
+						<a class="nav-link login" href="<?= site_url('auth/login') ?>">Login</a>
 					</li>
 				</ul>
 			</div>

3655224b3f4eb529b0b21c1151e2937bea8ba66f
Nrulafind <mnurula.nu246@gmail.com>
2022-07-08 19:18:24 +0700

    commit

--- application/controllers/Franchise.php
+++ application/controllers/Franchise.php
@@ -6,7 +6,7 @@ class Franchise extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        if ($this->session->userdata('status') === 'login') {
+        if ($this->session->userdata('status') !== 'login') {
             redirect('auth/login');
         }
         $this->load->model('FranchiseModel');

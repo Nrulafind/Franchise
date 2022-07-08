f55311fc878a8504af92279a60e3d0cb80d0925d
Nrulafind <mnurula.nu246@gmail.com>
2022-07-08 19:45:15 +0700

    commit

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,52 +1,55 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo base_url('./assets/css/access.css'); ?>">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>FriendChise</title>
-  </head>
-  <body class="text-center">
-     
-    <!-- Login Start -->
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url('./assets/css/access.css'); ?>">
 
-    <main class="form-signin">
-        <form action= "" method="post">
-          <img class="mb-4" src="<?php echo base_url('./assets/images/Logo-xl.png');?>" alt="" width="250" height="145">
-          <br><br><br><br>
+  <title>FriendChise</title>
+</head>
 
-      
-          <div class="form-floating">
-            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating">
-            <input name="email" type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-      
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me"> Remember me
-            </label>
-          </div>
-          <a href="<?php base_url();?>Homepage"><button class="w-100 btn btn-lg button" type="submit">Sign in</button></a>
-          <br><br>
-          <p>Don't have an account ? <a href="<?php base_url(); ?>auth/register" class="sgp">Sign Up</a></p>
+<body class="text-center">
+
+  <!-- Login Start -->
+
+  <main class="form-signin">
+    <form action="" method="post">
+      <img class="mb-4" src="<?php echo base_url('./assets/images/Logo-xl.png'); ?>" alt="" width="250" height="145">
+      <br><br><br><br>
 
 
-          <p class="mt-5 mb-3 text-muted">&copy; 2022 - FriendChise</p>
-        </form>
-      </main>
+      <div class="form-floating">
+        <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input name="email" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
 
-    <!-- Login End -->
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <a href="<?php base_url(); ?>Homepage"><button class="w-100 btn btn-lg button" type="submit">Sign in</button></a>
+      <br><br>
+      <p>Don't have an account ? <a href="<?php base_url(); ?>Auth/register" class="sgp">Sign Up</a></p>
 
-    <!-- Footer End -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
+
+      <p class="mt-5 mb-3 text-muted">&copy; 2022 - FriendChise</p>
+    </form>
+  </main>
+
+  <!-- Login End -->
+
+  <!-- Footer End -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
 </html>

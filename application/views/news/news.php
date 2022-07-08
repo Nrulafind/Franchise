e875221d3ff3c159b61b14baa5cab492768c5fed
digitalmediatnt <100731265+digitalmediatnt@users.noreply.github.com>
2022-07-08 21:36:32 +0700

    commit

---
+++ application/views/news/news.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo base_url('./assets/css/style.css'); ?>">
+
+	<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="8bb95c6d-9134-49c4-b9df-ab402a708c6d";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+	<title>FriendChise</title>
+</head>
+
+<body>
+
+	<!-- Navbar Start -->
+
+	<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top ">
+		<div class="container">
+			<a class="navbar-brand" href="index.html">
+				<h4>FriendChise</h4>
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item">
+						<a class="nav-link active " aria-current="page" href="<?php base_url() ?>Welcome">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php base_url() ?>Franchise">Franchise</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php base_url(); ?>Profile">Profile</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link login" href="<?php base_url() ?>Auth/login">Login</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<!-- Navbar End-->
+
+	<!-- Header Start -->
+
+	<div class="container px-4 py-5">
+		<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+			<div class="col-10 col-sm-8 col-lg-6 position-relative">
+				<img src="assets/images/HeaderImg.png" class="d-block mx-auto img-fluid img-header" alt="Img1" width="700" height="500" loading="lazy">
+			</div>
+			<div class="col-lg-6">
+				<h1 class="display-5 fw-lighter lh-1 mb-3 title-head">Find your best <span>FranChise</span></h1>
+				<p class="lead text-head">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo consequatur sunt incidunt obcaecati debitis sequi amet perspiciatis explicabo, expedita nemo?</p>
+				<div class="d-grid gap-2 d-md-flex justify-content-md-start">
+					<a href="<?php base_url() ?>Franchise"><button type="button" class="btn btn-lg px-6 me-md-2 button-header outline-none button-head">Franchise</button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Header End -->
+
+	<!-- Body Content Start -->
+        <div class="page">
+            <h2>Buka Forum Waralaba, Mendag Ungkap Industri Franchise Serap 628 Ribu Tenaga Kerja di 2020</h2>
+            <p>Dampak pandemi COVID-19 terhadap dunia usaha, masih dirasakan hingga saat ini, termasuk pada sektor waralaba. Kondisi ini membuat para pelaku usaha waralaba harus mampu beradaptasi dengan kondisi new normal, di mana masyarakat lebih banyak beraktivitas di rumah.
+            <br>Meskipun demikian, Menteri Perdagangan Muhammad Lutfi menyampaikan, pelaku usaha harus tetap optimistis karena pada 2020, bisnis waralaba di Indonesia masih tetap dapat berkontribusi dalam penyerapan 628 ribu tenaga kerja dan mencatat omzet tidak kurang dari Rp 54,4 miliar.
+            <br>Hal itu Mendag Lutfi sampaikan dalam sebuah pesan video yang disiarkan di acara Indonesia Franchise Forum, disiarkan secara virtual pada Selasa (7/12/2021).
+            <br>"Pemerintah juga terus mendorong pengembangan bisnis waralaba dalam negeri, karena potensi pasar Indonesia yang besar dan menjanjikan," kata Mendag Lutfi.
+            <br>"Saya sangat bangga bahwa saat ini Indonesia tidak lagi menjadi pasar bagi waralaba asing, karena waralaba lokal sudah menjadi tuan rumah dan menguasai pasar dalam negeri bahkan mulai merambah ke pasar regional," lanjutnya.
+            <br>Mendag pun optimis dengan adanya kolaborasi antara pemerintah, dunia usaha, asosiasi waralaba di Indonesia dapat menguasai, menembus, dan bersaing di pasar global.</p>
+
+            <h2>Prospek Bisnis Waralaba 2022 Kian Menggiurkan </h2>
+            <p> Bisnis waralaba, lisensi, dan kemitraan mampu membuktikan kelihaiannya untuk melewati masa pandemi. Sempat terdampak pada masa awal pandemi Covid 19, kini para pelaku usaha tersebut kian adaptif sehingga bisnis yang mereka kembangkan makin menggeliat.
+            <br>Tri Raharjo, Ketua Umum Perhimpunan Waralaba & Lisensi Indonesia (WALI) mengatakan sejak pandemi mulai melandai, permintaan masyarakat terhadap produk barang dan jasa kian melonjak. Kondisi ini ditangkap sebagai peluang bagi para investor untuk menawarkan bisnis waralaba, lisensi, dan kemitraan.
+            <br>Tentu saja ketika bisnis ini makin menggeliat maka akan memberikan multiplier effect yang besar di berbagai sektor ekonomi. Berdasarkan data dari Perhimpunan Waralaba & Lisensi Indonesia (Wali), pada awal pandemi tercatat ada 983 (17 persen) dari 5.621 gerai yang dimiliki oleh 30 brand waralaba, lisensi, dan kemitraan harus menutup usaha mereka secara sementara atau permanen</p>
+        
+            <h2>Pengelola Pizza Hut (PZZA) Berbalik Untung Rp60,76 Miliar pada 2021</h2>
+            <p> PT Sarimelati Kencana Tbk. (PZZA) membukukan keuntungan pada 2021. Setelah mengalami rugi Rp93,51 miliar pada 2020, perusahaan pemegang hak waralaba Pizza Hut tersebut berhasil mencetak laba bersih tahun berjalan sebesar Rp60,76 miliar pada 2021.
+            <br>PIZZA meraup laba bersih meski penjualan tercatat menurun. Berdasarkan laporan keuangan perusahaan, Kamis (7/4/2022), penjualan bersih PZZA turun 1,14 persen YoY menjadi menjadi Rp3,41 triliun, dari Rp3,45 triliun pada 2020.
+            <br>Berdasarkan segmen produk, penjualan makanan dan minuman PZZA sebelum dikurangi potongan kompak turun. Penjualan makanan turun 1,12 persen YoY menjadi Rp3,25 triliun, dari Rp3,28 triliun pada tahun sebelumnya. Sementara itu, penjualan minuman terkoreksi 3,84 persen YoY dari Rp181,43 miliar pada 2020 menjadi Rp174,46 miliar di 2021</p>
+
+        </div>
+
+    
+	<!-- Body Content End -->
+
+	<!-- Footer Start -->
+
+	<div class="container footer">
+		<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+			<div class="col-md-3 d-flex align-items-center footer copyright">
+				<span class="copyright"> &copy;<script>
+						document.write(new Date().getFullYear())
+					</script> - FriendChise</span>
+			</div>
+
+			<ul class="nav col-md-4 justify-content-end list-unstyled d-flex footer sosmed">
+				<li class="ms-3"><a href="#"><i class="fa-brands fa-instagram fa-lg"></i></a></li>
+				<li class="ms-3"><a href="#"><i class="fa-brands fa-tiktok fa-lg"></i></svg></a></li>
+				<li class="ms-3"><a href="#"><i class="fa-brands fa-twitter fa-lg"></i></a></li>
+			</ul>
+		</footer>
+	</div>
+
+	<!-- Footer End -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<script src="https://kit.fontawesome.com/25af777db1.js" crossorigin="anonymous"></script>
+</body>
+
+</html>

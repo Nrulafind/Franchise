1d4627632fd3dfc441acdb68f8a648d998451be0
Nrulafind <mnurula.nu246@gmail.com>
2022-07-08 20:22:43 +0700

    commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,6 +4,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Auth extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('AuthModel');
+    }
+
     public function login()
     {
         $e = $this->input->post('email');

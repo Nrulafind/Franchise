030e2ba32b57da50fb61192526f2ef56cacbc810
Nrulafind <mnurula.nu246@gmail.com>
2022-07-08 20:12:27 +0700

    commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -25,7 +25,7 @@ class Auth extends CI_Controller
             $this->session->set_userdata($data_session);
             redirect('Homepage');
         } else {
-            redirect('welcome');
+            redirect('');
             json_encode('INVALID_LOGIN');
         }
     }

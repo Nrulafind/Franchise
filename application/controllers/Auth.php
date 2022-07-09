6b349e0561198556d3edbd21e378a02be785a98d
Nrulafind <mnurula.nu246@gmail.com>
2022-07-09 09:49:34 +0700

    commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,12 +10,12 @@ class Auth extends CI_Controller
         $this->load->model('AuthModel');
     }
 
-    public function login()
+    public function login_user()
     {
         $e = $this->input->post('email');
         $p = $this->input->post('password');
 
-        $cek = $this->AuthModel->cek_login($e, $p)->num_rows();
+        $cek = $this->AuthModel->cek_login_user($e, $p)->num_rows();
         if ($cek == 1) {
             $data_session = array(
                 'email' => $e,
@@ -25,12 +25,32 @@ class Auth extends CI_Controller
             $this->session->set_userdata($data_session);
             $this->load->view('Homepage/homepage');
         } else {
-            $this->load->view('Auth/login');
+            $this->load->view('auth/login');
             json_encode('INVALID_LOGIN');
         }
     }
 
-    public function register()
+    public function login_admin()
+    {
+        $e = $this->input->post('email');
+        $p = $this->input->post('password');
+
+        $cek = $this->AuthModel->cek_login_admin($e, $p)->num_rows();
+        if ($cek == 1) {
+            $data_session = array(
+                'email' => $e,
+                'password' => $p,
+                'status' => 'login'
+            );
+            $this->session->set_userdata($data_session);
+            $this->load->view('Homepage/homepage');
+        } else {
+            $this->load->view('auth/login');
+            json_encode('INVALID_LOGIN');
+        }
+    }
+
+    public function register_user()
     {
         if ($this->session->userdata('status') === 'login') {
             redirect('auth/login');
@@ -38,7 +58,25 @@ class Auth extends CI_Controller
         if ($email = $this->input->post('email')) {
             $username = $this->input->post('username');
             $password = $this->input->post('password');
-            if ($user = $this->auth_model->register($email, $username, $password)) {
+            if ($user = $this->AuthModel->register_user($email, $username, $password)) {
+                $this->session->set_userdata($user);
+                echo json_encode('sukses');
+            } else {
+                echo json_encode('gagal');
+            }
+        } else {
+            $this->load->view('auth/register');
+        }
+    }
+    public function register_admin()
+    {
+        if ($this->session->userdata('status') === 'login') {
+            redirect('auth/login');
+        }
+        if ($email = $this->input->post('email')) {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            if ($user = $this->AuthModel->register_admin($email, $username, $password)) {
                 $this->session->set_userdata($user);
                 echo json_encode('sukses');
             } else {
@@ -52,6 +90,6 @@ class Auth extends CI_Controller
     public function logout()
     {
         $this->session->sess_destroy();
-        redirect('HomepageBefore');
+        redirect('welcome_message');
     }
 }
--- application/models/AuthModel.php
+++ application/models/AuthModel.php
@@ -4,34 +4,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class AuthModel extends CI_Model
 {
 
-    public function cek_login($e, $p)
+    public function cek_login_user($e, $p)
     {
         $q = $this->db->get_where('user', array('email' => $e, 'password' => $p));
         return $q;
     }
 
-    // public function login($email, $password)
-    // {
-    //     $this->db->where('email', $email);
-    //     if ($user = $this->db->get($this->table)->row()) {
-    //         if (password_verify($password, $user->password)) {
-    //             $data = array(
-    //                 'email' => $user->email,
-    //                 'password' => $user->password,
-    //                 'id_user' => $user->id_user,
-    //                 'status' => 'login'
-    //             );
-    //             $this->session->set_userdata($data);
-    //             return 'sukses';
-    //         } else {
-    //             redirect('Auth');
-    //         }
-    //     } else {
-    //         redirect('welcome');
-    //     }
-    // }
+    public function cek_login_admin($e, $p)
+    {
+        $q = $this->db->get_where('admin', array('email' => $e, 'password' => $p));
+        return $q;
+    }
 
-    public function register($email, $username, $password)
+    public function register_user($email, $username, $password)
     {
         $data = array(
             'email' => $email,
@@ -40,6 +25,19 @@ class AuthModel extends CI_Model
         );
         $this->db->insert($this->table, $data);
         $data['id_user'] = $this->db->insert_id_user();
+        $data['status'] = 'login';
+        return $data;
+    }
+
+    public function register_admin($email, $username, $password)
+    {
+        $data = array(
+            'email' => $email,
+            'username' => $username,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        );
+        $this->db->insert($this->table, $data);
+        $data['id_user'] = $this->db->insert_id_admin();
         $data['status'] = 'login';
         return $data;
     }

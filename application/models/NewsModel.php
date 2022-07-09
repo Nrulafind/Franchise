4eb8eb92e55bd752d135f23edceff2037654c411
Nrulafind <mnurula.nu246@gmail.com>
2022-07-09 22:18:02 +0700

    commit

--- application/models/NewsModel.php
+++ application/models/NewsModel.php
@@ -3,10 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class NewsModel extends CI_Model
 {
-    // protected $table = 'product';
 
     // public function get_all_news()
     // {
     //     $this->db->where();
     // }
+    public function insert_news($all)
+    {
+    }
 }
